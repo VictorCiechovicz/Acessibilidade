aead055cb423aeb9445433110cdac7106fd20c6b
carlosbrigo <brigo.d@gmail.com>
2023-06-05 22:12:37 -0300

    Banco

--- src/config/database-connection.js
+++ src/config/database-connection.js
@@ -14,16 +14,18 @@ Local.init(sequelize);
 //Associar em ordem 
 Local.associate(sequelize.models);
 
-databaseInserts(); 
+//Para para de criar automatico
+databaseInserts();
 
 function databaseInserts() {
     (async () => {
 
-        await sequelize.sync({ force: true }); 
+        await sequelize.sync({ force: true });
 
-        //const Local1 = await Local.create({ id_empresa: 1, nome_sabor: "1", valor: 1.00, id_imagem: 1, descricao: "teste 01", status: true, id_categoria: 1});
-        //const Local2 = await Local.create({ id_empresa: 1, nome_sabor: "2", valor: 2.00, id_imagem: 2, descricao: "teste 02", status: false, id_categoria: 2});
-       // const Local3 = await Local.create({ id_empresa: 1, nome_sabor: "3", valor: 2.00, id_imagem: 2, descricao: "teste 02", status: false, id_categoria: 2});
+        const Local1 = await Local.create({ predio: "A", andar: "1", sala: "12", descricao: "sala  12", qr: "TESTE" });
+        const Local2 = await Local.create({ predio: "A", andar: "1", sala: "12", descricao: "sala  12", qr: "TESTE" });
+        const Local3 = await Local.create({ predio: "A", andar: "1", sala: "12", descricao: "sala  12", qr: "TESTE" });
+        const Local4 = await Local.create({ predio: "A", andar: "1", sala: "12", descricao: "sala  12", qr: "TESTE" });
 
     })();
 }
